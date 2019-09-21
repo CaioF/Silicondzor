af6a37c10876a0eb82198885f553233a3c097ea3
Замалетдинова Альбина Арслановна <zamaletdinovaaa@sibur.ru>
2019-09-21 22:57:00 +0300

    bla bla

--- app-sillicondzor/src/App.js
+++ app-sillicondzor/src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Form from './components/form';
+import GraphLabel from './components/graphlabel';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="App">
+        <ul>
+          <li><Link to="/">HOME</Link></li>
+          <li><Link to="/info">INFO</Link></li>
+        </ul>
+        <header className="App-header">
+          <img className="logo-header" src={"logo-big.jpg"} alt="logo" />
+        </header>
+
+        <hr />
+
+        <Route exact path="/" component={Home} />
+        <Route path="/info" component={Info} />
+      </div>
+    </Router>
+  );
+}
+
+
+function Home() {
+  return (
+    <div className="home_container">
+      <Form />
+      <GraphLabel />
+    </div>
+  );
+}
+
+function Info() {
+  return (
+    <div>
+      <h2>Info</h2>
     </div>
   );
 }
