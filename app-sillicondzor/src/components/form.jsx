ee0802734fe2b5a67cdcbe4f69b5c0730f2ebeec
@CFleury <thecfrt@gmail.com>
2019-09-21 22:47:47 +0300

    removed node_modules

--- app-sillicondzor/src/components/form.jsx
+++ app-sillicondzor/src/components/form.jsx
@@ -3,30 +3,7 @@ import React, { Component } from 'react';
 export default class Form extends Component {
 
   handleClick(event) {
-    fetch('http://172.17.0.1:3000/send', {
-      body: 'send',
-      cache: 'no-cache',
-      credentials: 'same-origin',
-      headers: {
-        'content-type': 'application/json; charset=utf-8'
-      },
-      method: 'POST',
-      mode: 'cors',
-      redirect: 'follow',
-      referrer: 'no-referrer',
-    })
-      .then(function (response) {
-        console.log(response);
-        if (response.status === 200) {
-          setTimeout(function(){  window.location='http://172.17.0.1:3000/send' }, 700);
-        } else {
-          alert('Ошибка сервера!');
-        }
-        // you cannot parse your "success" response, since that is not a valid JSON
-        // consider using valid JSON req/resp pairs.
-        // return response.json();
-      });
-
+    setTimeout(function(){  window.location='http://10.55.130.239:3000/send' }, 700);
   }
 
   render() {
