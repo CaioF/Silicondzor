812064ca9e0071d3c829661bedfd548f93944e35
@CFleury <thecfrt@gmail.com>
2019-09-22 04:03:36 +0300

    added server

--- app2/index.js
+++ app2/index.js
@@ -6,9 +6,18 @@ const app = express();
 app.use(express.static(path.join(__dirname, '')));
 app.use(bodyParser.json());
 
-app.get('/*', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '', 'index.html'));
 });
+
+app.get('/analysis', function(req, res) {
+  res.sendFile(path.join(__dirname, '', 'analysis.html'));
+});
+
+app.get('/browsewells', function(req, res) {
+  res.sendFile(path.join(__dirname, '', 'browsewells.html'));
+});
+
 
 app.listen(8008);
 console.log("Server is running on port 8008, set to your local IP");
